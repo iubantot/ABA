230e82cae944af93045070cdf0e050616eaeaf4d
iubantot <iubantot@gmaill.com>
2016-08-28 00:21:55 +0800

    fixed folder locations

--- html/headers/header.php
+++ html/headers/header.php
@@ -8,9 +8,9 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span> 
       </button>
-		  <a class="navbar-brand" href="../uhack/">
+		  <a class="navbar-brand" href="../ABA/">
 		  <!--Logo here -->
-		  <img src="../uhack/img/logo.png" width="auto" height="100px">
+		  <img src="../ABA/img/logo.png" width="auto" height="100px">
 		  </a>
 		</div>
 	<div class="collapse navbar-collapse" id="myNavbar">
--- html/lib_config.php
+++ html/lib_config.php
@@ -3,8 +3,8 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <!-- Official Bootstrap -->
-  <link rel="stylesheet" href="../uhack/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="../ABA/bootstrap/css/bootstrap.min.css">
   <!-- Custom Style -->
-  <link rel="stylesheet" href="../uhack/css/style.css">
-  <script src="../uhack/bootstrap/jquery/jquery.min.js"></script>
-  <script src="../uhack/bootstrap/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="../ABA/css/style.css">
+  <script src="../ABA/bootstrap/jquery/jquery.min.js"></script>
+  <script src="../ABA/bootstrap/js/bootstrap.min.js"></script>
